f64ac163322b93557c4bb703fe4262346ba926c8
Premkumar981 <premkumarsuru@gmail.com>
2025-07-16 10:41:09 +0530

    updated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
+import orderRouter from "./routes/orderRoute.js"
 import cors from "cors";
 dotenv.config();
 const app = express();
@@ -12,19 +13,20 @@ app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-//     app.listen(8080, () => {
-//         console.log("Server started");
-//     });
-// });
-
-mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.zskdoqr.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
-    )
-    .then(() => {
+mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
     app.listen(8080, () => {
         console.log("Server started");
     });
 });
 
+// mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.zskdoqr.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+//     )
+//     .then(() => {
+//     app.listen(8080, () => {
+//         console.log("Server started");
+//     });
+// });
+
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
